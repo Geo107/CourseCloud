7861725aba8ab89262b1b5b9414cee5ac75b7788
Geo107 <geombenny@gmail.com>
2026-08-10 17:59:22 +0530

    LOGGING

--- coursecloud/settings.py
+++ coursecloud/settings.py
@@ -146,3 +146,19 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'},
+    },
+    'root': {'handlers': ['console'], 'level': 'WARNING'},
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
